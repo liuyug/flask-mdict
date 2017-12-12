d406012d971cdfb218b305f14ed85c0ee41111f3
LIU Yugang <liuyug@gmail.com>
2017-12-12 15:11:09 +0800

    add tdx history

--- his_quote.py
+++ his_quote.py
@@ -7,7 +7,8 @@ import argparse
 
 import six
 
-from stock.history.ths import quote
+from stock.history.ths import quote as ths
+from stock.history.tdx import quote as tdx
 
 
 def main():
@@ -17,7 +18,9 @@ def main():
     subparsers = argparser.add_subparsers(help='hq category', dest='category')
 
     parser = subparsers.add_parser('ths')
-    quote.add_arguments(parser)
+    ths.add_arguments(parser)
+    parser = subparsers.add_parser('tdx')
+    tdx.add_arguments(parser)
 
     args = argparser.parse_args()
 
@@ -25,7 +28,9 @@ def main():
         sys.stdout = codecs.getwriter(args.encoding)(sys.stdout)
 
     if args.category == 'ths':
-        quote.exec_args(args)
+        ths.exec_args(args)
+    elif args.category == 'tdx':
+        tdx.exec_args(args)
     else:
         argparser.print_help()
 
