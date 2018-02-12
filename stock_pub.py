18d556fd612b866bd5ec9d1f0b9eabdad2196466
LIU Yugang <liuyug@gmail.com>
2018-02-12 16:13:29 +0800

    fix

--- stock_pub.py
+++ stock_pub.py
@@ -6,7 +6,6 @@ import codecs
 import argparse
 import datetime
 
-import six
 from mtable import MarkupTable
 
 from stock.common import ths
@@ -16,27 +15,18 @@ from stock.common import cninfo
 def main(parser):
     args = parser.parse_args()
 
-    if six.PY2:
-        sys.stdout = codecs.getwriter(args.encoding)(sys.stdout)
-
-    today = datetime.date.today()
-    if args.day <= 0:
-        delta = datetime.timedelta.max
-    else:
-        delta = datetime.timedelta(days=args.day)
-    if args.news or args.pub:
+    if args.pub:
         for mcode in args.mcode:
-            data = ths.get_news(mcode)
-            if args.news:
-                for item in data['mine']:
-                    news_date = datetime.datetime.strptime(item['date'], '%Y-%m-%d').date()
-                    if (today - news_date) < delta:
-                        print('[%(date)s] %(title)s' % item)
-            elif args.pub:
-                for item in data['pub']:
-                    news_date = datetime.datetime.strptime(item['date'], '%Y-%m-%d').date()
-                    if (today - news_date) < delta:
-                        print('[%(date)s] %(title)s' % item)
+            data = cninfo.get_announcement(mcode)
+            table = MarkupTable()
+            table.set_dict_data(data, header=['date', 'type', 'size', 'title'])
+            print(table.to_rst())
+    elif args.news:
+        for mcode in args.mcode:
+            data = ths.get_news(mcode, 'news')
+            table = MarkupTable()
+            table.set_dict_data(data, header=['date', 'title'])
+            print(table.to_rst())
     elif args.period:
         for mcode in args.mcode:
             data = cninfo.get_pdf_report(mcode)
@@ -49,9 +39,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Search current stock news')
     parser.add_argument('-v', '--verbose', help='verbose help',
                         action='count', default=1)
-    parser.add_argument('--encoding', default='utf-8', help='Output terminal encoding. default: utf-8')
-
-    parser.add_argument('--day', type=int, default=0, help='news/public announcement before days')
 
     parser.add_argument('--news', action='store_true', help='news')
     parser.add_argument('--pub', action='store_true', help='public announcement')
