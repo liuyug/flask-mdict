802199c6bcd2b9c7aef012e7d13184bbad10b31e
LIU Yugang <liuyug@gmail.com>
2018-02-27 16:11:59 +0800

    add highcharts editor

--- runserver.sh
+++ runserver.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 export FLASK_APP=server.app
-export PYTHONPATH=$PYTHONPATH;`pwd`
+export PYTHONPATH="`pwd`:$PYTHONPATH"
 
 if [ "x$1" = "xrelease" ];then
     export FLASK_DEBUG=0
-    export APP_CONFIG_FILE="`pwd`/config/release.py"
+    export APP_CONFIG_FILE="../config/release.py"
 else
     export FLASK_DEBUG=1
-    export APP_CONFIG_FILE="`pwd`/config/debug.py"
+    export APP_CONFIG_FILE="../config/debug.py"
 fi
 
-echo $APP_CONFIG_FILE
+echo "python path: $PYTHONPATH"
+echo "config     : $APP_CONFIG_FILE"
 flask run
 
 # vim: tabstop=4 shiftwidth=4 expandtab
