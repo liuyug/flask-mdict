87f7eff7395218fbce7908f4c5cdd63c5380da2a
LIU Yugang <liuyug@gmail.com>
2018-04-29 20:54:01 +0800

    fix

--- finance_after.sql
+++ finance_after.sql
@@ -2,5 +2,5 @@ create index main_index on main ('stock_mcode');
 create index debt_index on debt ('stock_mcode');
 create index cash_index on cash ('stock_mcode');
 create index benefit_index on benefit ('stock_mcode');
-
+.index
 
