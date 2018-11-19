c938de4ff9d7730a31d8db1d6b5494b0c8abfbc3
LIU Yugang <liuyug@gmail.com>
2018-11-19 10:55:20 +0800

    fix and add tdx day format

--- stock_history.py
+++ stock_history.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding:utf-8 -*-
 
-import sys
-import codecs
 import argparse
-
-import six
 
 from stock.service.ths.local import quote as ths
 from stock.service.tdx.local import quote as tdx
@@ -13,8 +9,6 @@ from stock.service.tdx.local import quote as tdx
 
 def main():
     argparser = argparse.ArgumentParser(description='History HQ')
-    argparser.add_argument('--encoding', default='utf8', help='terminal character encoding')
-
     subparsers = argparser.add_subparsers(help='hq category', dest='category')
 
     parser = subparsers.add_parser('ths')
@@ -23,9 +17,6 @@ def main():
     tdx.add_arguments(parser)
 
     args = argparser.parse_args()
-
-    if six.PY2:
-        sys.stdout = codecs.getwriter(args.encoding)(sys.stdout)
 
     if args.category == 'ths':
         ths.exec_args(args)
