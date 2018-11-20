3773c246fb31018501494572c4b35041d1b71037
LIU Yugang <liuyug@gmail.com>
2018-11-20 13:31:44 +0800

    fix duplicated record

---
+++ quote_after.sql
@@ -0,0 +1,5 @@
+
+create index quote_day_index_mcode on day ('mcode');
+create index quote_day_index_mcode on min1 ('mcode');
+create index quote_day_index_mcode on min5 ('mcode');
+.index
---
+++ quote_before.sql
@@ -0,0 +1,5 @@
+
+.index
+drop index if exists quote_day_index_mcode;
+drop index if exists quote_min1_index_mcode;
+drop index if exists quote_min5_index_mcode;
