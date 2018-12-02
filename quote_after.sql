3db548e9a382f2934208326eec27966e64915e3e
LIU Yugang <liuyug@gmail.com>
2018-12-02 16:02:09 +0800

    fix name

--- quote_after.sql
+++ quote_after.sql
@@ -1,5 +1,5 @@
 
-create index quote_day_index_mcode on day ('mcode');
-create index quote_day_index_mcode on min1 ('mcode');
-create index quote_day_index_mcode on min5 ('mcode');
+create index quote_day_index_mcode on k_day ('mcode');
+create index quote_day_index_mcode on k_min1 ('mcode');
+create index quote_day_index_mcode on k_min5 ('mcode');
 .index
