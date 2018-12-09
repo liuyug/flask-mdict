5b6c950919b773854ace56032a8694be62dcc1f5
LIU Yugang <liuyug@gmail.com>
2018-12-09 11:18:34 +0800

    fix

--- finance_after.sql
+++ finance_after.sql
@@ -1,6 +1,6 @@
-create index main_index on main ('stock_mcode');
-create index debt_index on debt ('stock_mcode');
-create index cash_index on cash ('stock_mcode');
-create index benefit_index on benefit ('stock_mcode');
+create index main_index on main (stock_mcode);
+create index debt_index on debt (stock_mcode);
+create index cash_index on cash (stock_mcode);
+create index benefit_index on benefit (stock_mcode);
 .index
 
--- quote_after.sql
+++ quote_after.sql
@@ -1,5 +1,5 @@
 
-create index quote_day_index_mcode on k_day ('mcode');
-create index quote_day_index_mcode on k_min1 ('mcode');
-create index quote_day_index_mcode on k_min5 ('mcode');
+create index quote_day_index_mcode on k_day (mcode);
+create index quote_day_index_mcode on k_min1 (mcode);
+create index quote_day_index_mcode on k_min5 (mcode);
 .index
