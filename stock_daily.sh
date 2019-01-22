71be8caa99eef69376e4eca4e8a28fa522399b45
LIU Yugang <liuyug@gmail.com>
2019-01-23 05:32:14 +0800

    add bash

---
+++ stock_daily.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "x$1" = "x" ]; then
+    today=`date +%Y%m%d`
+else
+    today=$1
+fi
+echo "run it after $today 18:00"
+
+# import daily quote
+python3 stock_kline.py tdx --import-day-cod --cod-date $today
+
+# create tpo
+python3 web_admin.py --send-tpo --tpo-source db --output-dir ~/Desktop/temp
+
+ma="database/ma_$today.txt"
+# check ma
+python3 stock_analyst.py --ma --source db --output $ma -
+
+count=`cat $ma | wc -l`
+echo "$today,$count" >> "database/ma_stat.csv"
+cat "database/ma_stat.csv"
+
+
+
+# vim: tabstop=4 shiftwidth=4 expandtab
