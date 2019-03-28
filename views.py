9b73c8cf6829cdfdcf0558bdc1287a433c02b59c
LIU Yugang <liuyug@gmail.com>
2019-03-28 10:28:57 +0800

    compatible " and \' :(

--- views.py
+++ views.py
@@ -12,10 +12,10 @@ from . import helper
 
 
 regex_word_link = re.compile(r'^(@@@LINK=)(.+)$')
-regex_src_schema = re.compile(r'([ "]src=")(/|file:///)?(.+?")')
-regex_href_end_slash = re.compile(r'([ "]href=".+?)(/)(")')
-regex_href_schema = re.compile(r'([ "]href=")(sound://|entry://|http://|https://)([^#].+?")')
-regex_href_no_schema = re.compile(r'([ "]href=")(?!sound://|entry://|http://|https://)([^#].+?")')
+regex_src_schema = re.compile(r'([ "]src=["\'])(/|file:///)?(.+?["\'])')
+regex_href_end_slash = re.compile(r'([ "]href=["\'].+?)(/)(["\'])')
+regex_href_schema = re.compile(r'([ "]href=["\'])(sound://|entry://|http://|https://)([^#].+?["\'])')
+regex_href_no_schema = re.compile(r'([ "]href=["\'])(?!sound://|entry://|http://|https://)([^#].+?["\'])')
 
 
 @mdict.route('/query/<part>')
