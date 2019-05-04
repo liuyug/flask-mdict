ec1715748f635e1368c60dab18a429d2caef5cc1
LIU Yugang <liuyug@gmail.com>
2019-05-05 06:02:48 +0800

    download ths xls

--- collect_report.py
+++ collect_report.py
@@ -6,7 +6,7 @@ import logging
 
 from stock.base.stock import get_stock, get_plate
 from stock.service.ths.finance import import_finance_report
-from stock.service.ths.web.finance import download_finance_report
+from stock.service.ths.web.finance import download_finance_report, download_finance_data2
 from stock.service.tdx.local.finance import load_finance_data
 
 
@@ -31,7 +31,8 @@ def stock_main(parser):
         mcodes = [stock.mcode for stock in get_stock(market_code='SHSZA')]
 
     if args.download:
-        download_finance_report(mcodes, typ='json', overwrite=args.overwrite)
+        download_finance_data2(mcodes, typ='xls', overwrite=args.overwrite)
+        # download_finance_report(mcodes, typ='json', overwrite=args.overwrite)
     elif args.import_:
         import_finance_report(mcodes, typ='json')
     elif args.tdx:
