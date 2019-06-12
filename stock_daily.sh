7686e76abf2f3a4b2a51783d440cde27f5846950
LIU Yugang <liuyug@gmail.com>
2019-06-12 18:14:51 +0800

    fix

--- stock_daily.sh
+++ stock_daily.sh
@@ -11,16 +11,9 @@ echo "run it after $today 18:00"
 python3 stock_kline.py tdx --import-day-cod --cod-date $today
 
 # create tpo
-python3 web_admin.py --send-tpo --tpo-source db --output-dir ~/Desktop/temp
+python3 web_admin.py --send-tpo --tpo-source db --output-dir build
 
-ma="database/ma_$today.txt"
 # check ma
-python3 stock_analyst.py --ma --before-year 2 --source db --output $ma -
-
-count=`cat $ma | wc -l`
-echo "$today,$count" >> "database/ma_stat.csv"
-tail "database/ma_stat.csv"
-
-
+python3 stock_analyst.py --ma -
 
 # vim: tabstop=4 shiftwidth=4 expandtab
