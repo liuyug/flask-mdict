90045ced2c1e224ff2ba8dd8c07f02f439881f6b
Yugang LIU <liuyug@gmail.com>
2020-07-18 21:58:07 +0800

    fix app

--- app.py
+++ app.py
@@ -8,29 +8,21 @@ from flask import Flask, redirect, url_for
 
 
 def create_app():
-    if getattr(sys, 'frozen', False):
-        template_folder = os.path.join(sys._MEIPASS, 'templates')
-        static_folder = os.path.join(sys._MEIPASS, 'static')
-        app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
-    else:
-        app = Flask(__name__)
-    print(__file__)
+    app = Flask(__name__, template_folder=None, static_folder=None)
     app.config['MDICT_DIR'] = 'content'
     app.config['MDICT_CACHE'] = False
     app.config['SECRET_KEY'] = "21ffjfdlsafj2ofjaslfjdsaf"
 
-    from . import init_app
-    init_app(app)
+    from flask_mdict import init_app
+    init_app(app, url_prefix='/')
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return redirect(url_for('mdict.static', filename='logo.ico'))
 
     return app
 
 
 if __name__ == "__main__":
-    # fix import path
-    parent = os.path.dirname(os.path.realpath(__file__))
-    pparent = os.path.dirname(parent)
-    sys.path.append(pparent)
-    __package__ = os.path.basename(parent)
-
     app = create_app()
     app.run()
