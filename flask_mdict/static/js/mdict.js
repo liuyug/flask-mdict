8455147ad371d95ee0f86e0d47eb4006f6306e20
Yugang LIU <liuyug@gmail.com>
2020-07-28 14:20:25 +0800

    remove console.log

--- flask_mdict/static/js/mdict.js
+++ flask_mdict/static/js/mdict.js
@@ -1,7 +1,6 @@
 
 function mdict_click(element, event) {
     let url = element.href;
-    console.log(element, event);
     event.preventDefault();
     if (url.lastIndexOf('sound://', 0) === 0){
         if (element.hasAttribute('abs_url')) {
@@ -12,7 +11,6 @@ function mdict_click(element, event) {
         let mdict_player = document.createElement("audio");
         mdict_player.id = 'mdict_player';
         mdict_player.src = url;
-        console.log('play', url);
         mdict_player.play();
     } else if (url.lastIndexOf('entry://', 0) === 0){
         if (element.hasAttribute('abs_url')) {
